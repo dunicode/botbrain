bd16c2f853e1c8f0eef2eccfa339270fd9329d70
Hugo Camacho <hcamacho.int@gmail.com>
2025-10-31 16:12:50 -0400

    feat: environ os 2

--- conf/settings.py
+++ conf/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 import environ
 import os
+from pathlib import Path
 
 env = environ.Env()
 environ.Env.read_env()
@@ -128,7 +129,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_ROOT = 'static'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATIC_URL = '/static/'
 
 # Default primary key field type
