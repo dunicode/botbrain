7c3cd1b273af43dee03a5385a89185fa5f3aec64
Hugo Camacho <hcamacho.int@gmail.com>
2025-11-01 10:14:15 -0400

    feat: password

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,4 +1,44 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.forms import UserChangeForm, UserCreationForm
 from .models import User
 
-admin.site.register(User)
+class CustomUserChangeForm(UserChangeForm):
+    class Meta(UserChangeForm.Meta):
+        model = User
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = User
+        fields = ('email', 'username')  # Campos requeridos
+
+class CustomUserAdmin(UserAdmin):
+    form = CustomUserChangeForm
+    add_form = CustomUserCreationForm
+    
+    # Campos a mostrar en la lista del admin
+    list_display = ('email', 'username', 'is_staff', 'is_active')
+    list_filter = ('is_staff', 'is_active', 'groups')
+    
+    # Campos para la edición
+    fieldsets = (
+        (None, {'fields': ('email', 'username', 'password')}),
+        ('Información personal', {'fields': ('first_name', 'last_name')}),
+        ('Permisos', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Fechas importantes', {'fields': ('last_login', 'date_joined', 'created_at', 'updated_at')}),
+    )
+    
+    # Campos para la creación de usuario
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'username', 'password1', 'password2'),
+        }),
+    )
+    
+    search_fields = ('email', 'username', 'first_name', 'last_name')
+    ordering = ('email',)
+    filter_horizontal = ('groups', 'user_permissions',)
+
+# Registra el modelo User con la configuración personalizada
+admin.site.register(User, CustomUserAdmin)
